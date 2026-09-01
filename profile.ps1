18bcf9e087672fc64ff9dbd644ecf9d0533aead1
cmohan9 <cmohan9@deloitte.com>
2026-09-01 11:10:37 +0530

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -41,7 +41,24 @@ if ($SetId) {
         -Headers $Headers `
         -Body $Body
 
-    $Events | ConvertTo-Json -Depth 8
+    $CsvPath = "EPM_Events_$(Get-Date -Format 'yyyyMMdd_HHmmss').csv"
+
+    $FlatEvents = $Events.events | ForEach-Object {
+        $Row = [ordered]@{}
+        foreach ($Prop in $_.PSObject.Properties) {
+            if ($Prop.Value -is [System.Array] -or $Prop.Value -is [System.Management.Automation.PSCustomObject]) {
+                $Row[$Prop.Name] = ($Prop.Value | ConvertTo-Json -Depth 5 -Compress)
+            }
+            else {
+                $Row[$Prop.Name] = $Prop.Value
+            }
+        }
+        [PSCustomObject]$Row
+    }
+
+    $FlatEvents | Export-Csv -Path $CsvPath -NoTypeInformation -Encoding UTF8
+
+    Write-Host "Exported $($FlatEvents.Count) events to $CsvPath"
 }
 else {
     $Sets = Invoke-RestMethod `
